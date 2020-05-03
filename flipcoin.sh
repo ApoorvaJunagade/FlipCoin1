4e6065817b0e2319665281a6a5d440cbb6352802
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-03 15:57:05 +0530

    USECASE2: loop through multiple times

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,12 +1,20 @@
 #! /bin/bash -x
+
+i=0
+	while [ $i -le  21 ]
+	do
 		random=$(( $RANDOM%2 ))
 		
 
 		if [ $random -eq  1 ]
 		then
         		echo "Heads has won"
-		
+			
 		else
         		echo "Tails has won"
+			
 		fi
+		i=$(( $i +1 ))	
+	done
+	      
 	
